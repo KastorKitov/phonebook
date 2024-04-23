fe40c3051d52719a2f67be827751998baedbfb38
Kastor Kitov <kitov.kastor@gmail.com>
2024-04-23 15:24:52 +0300

    working add contact

---
+++ src/main/java/phonebook/controller/ContactController.java
@@ -0,0 +1,34 @@
+package phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import phonebook.entity.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ContactController {
+
+    List<Contact> contacts;
+
+    public ContactController() {
+        this.contacts = new ArrayList<>();
+    }
+
+    @GetMapping("/")
+    public ModelAndView home(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        modelAndView.addObject("contacts", this.contacts);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/")
+    public String add(Contact contact){
+        this.contacts.add(contact);
+        return "redirect:/";
+    }
+}
