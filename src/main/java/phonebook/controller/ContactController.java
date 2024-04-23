69c1e9deab977e516454afaa3908567c995c6227
Kastor Kitov <kitov.kastor@gmail.com>
2024-04-23 16:13:12 +0300

    working delete contact

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -1,21 +1,22 @@
 package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ContactController {
 
-    List<Contact> contacts;
+    Map<String,Contact> contacts;
 
     public ContactController() {
-        this.contacts = new ArrayList<>();
+        this.contacts = new LinkedHashMap<>();
     }
 
     @GetMapping("/")
@@ -28,7 +29,34 @@ public class ContactController {
 
     @PostMapping("/")
     public String add(Contact contact){
-        this.contacts.add(contact);
+        this.contacts.put(contact.getName(), contact);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit{name}")
+    public ModelAndView editForm(@PathVariable String name,ModelAndView modelAndView){
+        modelAndView.setViewName("edit");
+        modelAndView.addObject(this.contacts.get(name));
+
+        return modelAndView;
+    }
+
+    @PutMapping("/edit{name}")
+    public String edit(@PathVariable String name, Contact contact){
+
+        Contact contact1 = this.contacts.get(name);
+        contact1.setName(contact.getName());
+        contact1.setNumber(contact.getNumber());
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/delete{name}")
+    public String delete(@PathVariable String name){
+
+        this.contacts.remove(name);
+
+
         return "redirect:/";
     }
 }
